274abea03c9bfe1f5a1689f7da5c8dd88df7c2d2
Akmal Taufik <akmaltaufik123@users.noreply.github.com>
2026-08-13 21:04:26 +0800

    Wire Supabase anon key for portal

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,2 +1,2 @@
-window.SUPABASE_URL = "";
-window.SUPABASE_ANON_KEY = "";
+window.SUPABASE_URL = "https://lmvgxcdlnpckmqjjsqwf.supabase.co";
+window.SUPABASE_ANON_KEY = "sb_publishable_DbZorD8yGZII1lzddXgABA_tPaWlkMh";
